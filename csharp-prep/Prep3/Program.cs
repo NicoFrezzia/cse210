ab3957abe283d5d6f242329fb1a3ad641d2f94be
nicofrezzia <frezzia1629@gmail.com>
2025-01-16 23:24:58 -0700

    Made the guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Data;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Asking the user for the magic number and storing the number
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1,56);
+
+        int guess = 0;
+        int numberOfGuesses = 0;
+
+        while (guess != magicNumber){
+            Console.Write("What is your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber){
+                System.Console.WriteLine("Higher");
+                numberOfGuesses++;
+            } else if (guess > magicNumber){
+                System.Console.WriteLine("Lower");
+                numberOfGuesses++;
+            }
+        }
+        System.Console.WriteLine("You got the number! ");
+        System.Console.WriteLine($"It took you {numberOfGuesses} guesses! ");
+
+        
+
+
+
     }
+
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        string name;
+        Console.Write("Enter name: ");
+        name = Console.ReadLine();
     }
 }
